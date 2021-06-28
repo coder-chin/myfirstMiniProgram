b5185c2a8855949527ca20037c10edd1dcc8a402
qhy <1277180540@qq.com>
2021-06-28 18:12:56 +0800

    index finish

---
+++ data/json.js
@@ -0,0 +1,28 @@
+var json = [
+  {
+      "id": "75",
+      "title": "学百年党史 · 讲百个故事 | 伟大征程上的伟大转折",
+      "author": "BJFU",
+      "picurl": "http://mmbiz.qpic.cn/mmbiz_png/3XKXN9oWy8bPrAZGJnISMICtvlZlg1E6ibkCgztCb8mSibJ10HJKzFz91mXRmDxEXGRB6j0UuQDBuScSATxHCR1g/0?wx_fmt=png",
+      "posttime": "1624723200"
+  },
+  {
+      "id": "73",
+      "title": "毕业礼物，上！新！了！",
+      "author": "BJFU",
+      "picurl": "http://mmbiz.qpic.cn/mmbiz_png/3XKXN9oWy8ZfEansS7sibaT7CTtM6qVLxXTzoGlrfTf0BBROicP2hVYfCfTyea446YCclDS7Y44thWciaEgh0ZERQ/0?wx_fmt=png",
+      "posttime": "1624636800"
+  },
+  {
+      "id": "71",
+      "title": "高考出分！欢迎报考北京林业大学！",
+      "author": "BJFU",
+      "picurl": "http://mmbiz.qpic.cn/mmbiz_jpg/3XKXN9oWy8aKdeJAQGry8CRgsxGiaSmOAJsQYfX1j54B3fQ3VRnNtEUFNhDgic7H8xEVTGu3LGwXHR0k9maAVGqw/0?wx_fmt=jpeg",
+      "posttime": "1624464000"
+  }
+]
+
+//导出
+module.exports = {
+  dataList: json
+}
