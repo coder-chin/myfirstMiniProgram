282c3cdd7ef33f0f96395e9abb404f3ede612b32
qhy <1277180540@qq.com>
2021-06-29 11:53:18 +0800

    index finish

--- data/json.js
+++ data/json.js
@@ -1,4 +1,4 @@
-var json = [
+var newsjson = [
   {
       "id": "75",
       "title": "学百年党史 · 讲百个故事 | 伟大征程上的伟大转折",
@@ -22,7 +22,59 @@ var json = [
   }
 ]
 
+var college = [
+  {
+    "id": "1",
+    "imgsrc": "http://zsb.bjfu.edu.cn/userfiles/0d2799b8f37a43caae678e980659842e/images/cms/article/2018/06/%E6%9E%97%E5%AD%A6%E7%B1%BB.jpg",
+    "name": "林学院",
+    "englishName": "College of Forestry"
+  },
+  {
+    "id": "2",
+    "imgsrc": "http://zsb.bjfu.edu.cn/userfiles/0d2799b8f37a43caae678e980659842e/images/cms/article/2018/06/%E5%9C%B0%E7%90%86%E4%BF%A1%E6%81%AF%E7%A7%91%E5%AD%A6.jpg",
+    "name": "水土保持学院",
+    "englishName": "College of Soil and Water Conservation"
+  },
+  {
+    "id": "3",
+    "imgsrc": "http://zsb.bjfu.edu.cn/userfiles/0d2799b8f37a43caae678e980659842e/images/cms/article/2018/06/IMG_1883.jpg",
+    "name": "理学院",
+    "englishName": "College of Forestry"
+  },
+  {
+    "id": "4",
+    "imgsrc": "http://zsb.bjfu.edu.cn/userfiles/0d2799b8f37a43caae678e980659842e/images/cms/article/2018/06/IMG_1894.jpg",
+    "name": "园林学院",
+    "englishName": "College of Garden"
+  },
+  {
+    "id": "5",
+    "imgsrc": "http://zsb.bjfu.edu.cn/userfiles/0d2799b8f37a43caae678e980659842e/images/cms/article/2018/06/7(3).jpg",
+    "name": "经济管理学院",
+    "englishName": "College of economics and management"
+  },
+  {
+    "id": "6",
+    "imgsrc": "http://zsb.bjfu.edu.cn/userfiles/0d2799b8f37a43caae678e980659842e/images/cms/article/2018/06/IMG_2057.jpg",
+    "name": "工学院",
+    "englishName": "College of Engineerin"
+  },
+  {
+    "id": "6",
+    "imgsrc": "http://zsb.bjfu.edu.cn/userfiles/0d2799b8f37a43caae678e980659842e/images/cms/article/2018/06/IMG_2073.jpg",
+    "name": "信息学院",
+    "englishName": "College of IT"
+  },
+  {
+    "id": "6",
+    "imgsrc": "http://zsb.bjfu.edu.cn/userfiles/646284216cf74ff5baaf6fe1da4a247a/images/cms/article/2021/05/094A1396.jpg",
+    "name": "艺术设计学院",
+    "englishName": "College of Art Design"
+  }
+]
+
 //导出
 module.exports = {
-  dataList: json
+  dataList: newsjson,
+  collegeList: college
 }
